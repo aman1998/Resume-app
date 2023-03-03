c978da71dd95576c651e2b21a1b889b4aea3954c
Aman1998 <1998.amangeldi@gmail.com>
2023-03-04 00:26:51 +0600

    refactoring: some fixed

--- src/modules/Templates/MainTemplate/components/EducationsInfo/index.tsx
+++ src/modules/Templates/MainTemplate/components/EducationsInfo/index.tsx
@@ -8,6 +8,8 @@ import { educationsInfoSelector } from '@modules/UserInfo/store/selectors';
 
 import InfoItem from '../InfoItem';
 
+import styles from './educations.module.scss';
+
 const TemplateEducationsInfo: FC<{ withColors: boolean }> = ({ withColors }) => {
   const educations = useSelector(educationsInfoSelector);
 
@@ -16,11 +18,17 @@ const TemplateEducationsInfo: FC<{ withColors: boolean }> = ({ withColors }) => 
       <InfoHeader title="Образование" withColors={withColors} />
       {!!educations?.length &&
         educations.map((item) => (
-          <div key={item.id} style={{ marginBottom: 16 }}>
-            <InfoItem title="Тип образования" text={item.type} />
-            <InfoItem title="Учебное заведение" text={item.educationName} />
-            <InfoItem title="Факультет" text={item.faculty} />
-            <InfoItem title="Годы обучения" text={getIntervalYears(item.startYear, item.endYear)} />
+          <div key={item.id} className={styles['educations-item']}>
+            <div className={styles['educations__circle']} />
+            <div key={item.id}>
+              <InfoItem title="Тип образования" text={item.type} />
+              <InfoItem title="Учебное заведение" text={item.educationName} />
+              <InfoItem title="Факультет" text={item.faculty} />
+              <InfoItem
+                title="Годы обучения"
+                text={getIntervalYears(item.startYear, item.endYear)}
+              />
+            </div>
           </div>
         ))}
       {/* <InfoItem title="Гражданство" text={info?.nationality || 'Не указано'} /> */}
