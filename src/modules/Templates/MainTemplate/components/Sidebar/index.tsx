ee01c8523d0d37fcc3c22326f3a3526087b58b00
Aman1998 <1998.amangeldi@gmail.com>
2023-03-04 00:02:29 +0600

    refactoring: some fixed

--- src/modules/Templates/MainTemplate/components/Sidebar/index.tsx
+++ src/modules/Templates/MainTemplate/components/Sidebar/index.tsx
@@ -14,7 +14,7 @@ const TemplateSidebar: FC<{ withColors: boolean }> = ({ withColors }) => {
 
   return (
     <div className={classNames(styles.sidebar, { [styles['sidebar--colors']]: withColors })}>
-      {user?.personal.photoUrl ? (
+      {user?.personal?.photoUrl ? (
         <Image
           src={user.personal.photoUrl}
           width={withColors ? 220 : 210}
@@ -22,44 +22,48 @@ const TemplateSidebar: FC<{ withColors: boolean }> = ({ withColors }) => {
           alt="avatar"
         />
       ) : (
-        <div className={styles['sidebar__not-image']} />
+        <div
+          className={classNames(styles['sidebar__not-image'], {
+            [styles['sidebar__not-image--colors']]: withColors,
+          })}
+        />
       )}
       <div className={styles['sidebar__info']}>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Должность:</div>
-          <div className={styles['info-item__text']}>{user?.speciality.profession}</div>
+          <div className={styles['info-item__text']}>{user?.speciality?.profession}</div>
         </div>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Зарплата:</div>
           <div className={styles['info-item__text']}>
-            {user?.speciality.salary}{' '}
+            {user?.speciality?.salary}{' '}
             {user?.speciality?.salaryСurrency && getSalaryType(user.speciality.salaryСurrency)}
           </div>
         </div>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Телефон:</div>
-          <div className={styles['info-item__text']}>{user?.contacts.phone}</div>
+          <div className={styles['info-item__text']}>{user?.contacts?.phone}</div>
         </div>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Почта:</div>
-          <div className={styles['info-item__text']}>{user?.contacts.email}</div>
+          <div className={styles['info-item__text']}>{user?.contacts?.email}</div>
         </div>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Переезд:</div>
           <div className={styles['info-item__text']}>
-            {user?.speciality.relocatioReady ? 'Да' : 'Нет'}
+            {user?.speciality?.relocatioReady ? 'Да' : 'Нет'}
           </div>
         </div>
         <div className={styles['info-item']}>
           <div className={styles['info-item__title']}>Удаленная работа:</div>
           <div className={styles['info-item__text']}>
-            {user?.speciality.remoteWorkingReady ? 'Да' : 'Нет'}
+            {user?.speciality?.remoteWorkingReady ? 'Да' : 'Нет'}
           </div>
         </div>
       </div>
       <div className={styles['sidebar-skills']}>
         <h2 className={styles['sidebar-skills__title']}>Навыки</h2>
-        {user?.speciality.skills.length &&
+        {user?.speciality?.skills?.length &&
           user.speciality.skills.map((item) => <p key={item.text}>- {item.text}</p>)}
       </div>
     </div>
